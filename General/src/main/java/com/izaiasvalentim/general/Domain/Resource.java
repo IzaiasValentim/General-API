9b12583cef541e6018e5caf6fded9ed1640315e4
IzaiasValentim <izaias451@gmail.com>
2025-01-16 13:20:20 -0300

    feat/ Add the params for Resource class.

--- General/src/main/java/com/izaiasvalentim/general/Domain/Resource.java
+++ General/src/main/java/com/izaiasvalentim/general/Domain/Resource.java
@@ -1,4 +1,71 @@
 package com.izaiasvalentim.general.Domain;
 
+import java.util.List;
+
 public class Resource {
+    private long id;
+    private String name;
+    private List<Item> items;
+    private String itemCode;
+    private Double stock;
+
+    public Resource(List<Item> items, Double stock) {
+        this.items = items;
+        this.stock = stock;
+    }
+
+    public Resource() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName() {
+        if (!this.items.isEmpty()) {
+            this.name = this.items.getFirst().getName();
+
+        } else {
+            // É para gerar uma exception personalizada.
+            this.name = "";
+        }
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode() {
+        if (!this.items.isEmpty()) {
+            this.itemCode = this.items.getFirst().getCode();
+        } else {
+            // É para gerar uma exception personalizada.
+            this.itemCode = "";
+        }
+    }
+
+    public Double getStock() {
+        return stock;
+    }
+
+    public void setStock(Double stock) {
+        this.stock = stock;
+    }
 }
+
