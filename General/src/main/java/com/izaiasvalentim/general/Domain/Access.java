6d6fef1ee8b1bbd5ee139c0762a8e9f7cff3844b
IzaiasValentim <izaias451@gmail.com>
2025-01-16 13:10:38 -0300

    feat/ Add the params for Access class.

--- General/src/main/java/com/izaiasvalentim/general/Domain/Access.java
+++ General/src/main/java/com/izaiasvalentim/general/Domain/Access.java
@@ -1,4 +1,41 @@
 package com.izaiasvalentim.general.Domain;
 
+import java.util.Date;
+
 public class Access {
+    private long id;
+    private ApiUser user;
+    private Date date;
+
+    public Access(ApiUser user, Date date) {
+        this.user = user;
+        this.date = date;
+    }
+
+    public Access() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public ApiUser getUser() {
+        return user;
+    }
+
+    public void setUser(ApiUser user) {
+        this.user = user;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
