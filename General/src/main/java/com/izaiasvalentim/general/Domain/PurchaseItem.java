f13f1234c21a654f43e2e45aa28509a1a4450f77
IzaiasValentim <izaias451@gmail.com>
2025-01-16 13:20:11 -0300

    feat/ Add the params for PurchaseItem class.

--- General/src/main/java/com/izaiasvalentim/general/Domain/PurchaseItem.java
+++ General/src/main/java/com/izaiasvalentim/general/Domain/PurchaseItem.java
@@ -1,5 +1,52 @@
 package com.izaiasvalentim.general.Domain;
 
+import java.util.UUID;
 
 public class PurchaseItem {
+
+    private UUID id;
+    private Resource item;
+    private Purchase purchase;
+    private long quantity;
+
+    public PurchaseItem(Resource item, long quantity) {
+        this.item = item;
+        this.quantity = quantity;
+    }
+
+    public PurchaseItem() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Resource getItem() {
+        return item;
+    }
+
+    public void setItem(Resource item) {
+        this.item = item;
+    }
+
+    public Purchase getPurchase() {
+        return purchase;
+    }
+
+    public void setPurchase(Purchase purchase) {
+        this.purchase = purchase;
+    }
+
+    public long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(long quantity) {
+        this.quantity = quantity;
+    }
 }
+
