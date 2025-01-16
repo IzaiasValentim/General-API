cd07681f8c69a142d4cb187707e1e5273551c865
IzaiasValentim <izaias451@gmail.com>
2025-01-16 13:19:24 -0300

    feat/ Add the params for Company class.

--- General/src/main/java/com/izaiasvalentim/general/Domain/Company.java
+++ General/src/main/java/com/izaiasvalentim/general/Domain/Company.java
@@ -1,4 +1,59 @@
 package com.izaiasvalentim.general.Domain;
 
 public class Company {
+    private int id;
+    private String name;
+    private String brach;
+    private String address;
+    private String typeOfService;
+
+    public Company(String name, String brach, String address, String typeOfService) {
+        this.name = name;
+        this.brach = brach;
+        this.address = address;
+        this.typeOfService = typeOfService;
+    }
+
+    public Company() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrach() {
+        return brach;
+    }
+
+    public void setBrach(String brach) {
+        this.brach = brach;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTypeOfService() {
+        return typeOfService;
+    }
+
+    public void setTypeOfService(String typeOfService) {
+        this.typeOfService = typeOfService;
+    }
 }
