5f08fbf92d7ca8ec411d95b8900ffe7d9f7f371c
IzaiasValentim <izaias451@gmail.com>
2025-01-17 14:21:01 -0300

    feat/ Added the base class for Item services.

---
+++ General/src/main/java/com/izaiasvalentim/general/Service/ItemService.java
@@ -0,0 +1,17 @@
+package com.izaiasvalentim.general.Service;
+
+import com.izaiasvalentim.general.Repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Autowired
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+}
