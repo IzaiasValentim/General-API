8b54e99bd6a0bb8f99f2db004e2959033ead7e34
IzaiasValentim <izaias451@gmail.com>
2025-01-20 15:45:23 -0300

    fix/ The generated batch is applied by reference.

--- General/src/main/java/com/izaiasvalentim/general/Common/utils/ItemUtils.java
+++ General/src/main/java/com/izaiasvalentim/general/Common/utils/ItemUtils.java
@@ -5,12 +5,11 @@ import com.izaiasvalentim.general.Domain.Item;
 public class ItemUtils {
 
 
-    public static Item generateItemBatch(Item item){
+    public static void generateItemBatch(Item item){
         if (item.getName() == null || item.getName().length() < 2) {
             throw new IllegalArgumentException("O nome do item deve ter pelo menos 2 caracteres.");
         }
         String prefixo = item.getName().substring(0, 2).toUpperCase();
         item.setBatch(prefixo + item.getId());
-        return item;
     }
 }
