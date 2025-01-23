ba73220f93a2866cb6b1587e9565ca93d4792041
IzaiasValentim <izaias451@gmail.com>
2025-01-23 16:32:41 -0300

    fix/ Removed the annotation that makes JPA define the id automatically. In this case, the expectation is that there will be only one occurrence and always of id = 1.

--- General/src/main/java/com/izaiasvalentim/general/Domain/Company.java
+++ General/src/main/java/com/izaiasvalentim/general/Domain/Company.java
@@ -5,7 +5,6 @@ import jakarta.persistence.*;
 @Entity
 public class Company {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int id;
     @Column(unique = true)
     private String name;
