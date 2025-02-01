8096c7db200c1ee0ea178898387eabb4c674d8e1
IzaiasValentim <izaias451@gmail.com>
2025-02-01 12:36:02 -0300

    feat/ Creation of the controller for users.  * It's important to note that some methods are temporary and only used to validate functionalities. Such as getUserByUsername, which returns an object containing sensitive data. These will be removed in the future.

--- General/src/main/java/com/izaiasvalentim/general/Controller/ApiUserController.java
+++ General/src/main/java/com/izaiasvalentim/general/Controller/ApiUserController.java
@@ -1,9 +1,46 @@
 package com.izaiasvalentim.general.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.izaiasvalentim.general.Domain.ApiUser;
+import com.izaiasvalentim.general.Domain.DTO.ApiUser.ApiUserRegisterDTO;
+import com.izaiasvalentim.general.Service.ApiUserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "api/ApiUsers")
+@RequestMapping(value = "api/users")
 public class ApiUserController {
+
+    private final ApiUserService apiUserService;
+
+    public ApiUserController(ApiUserService apiUserService) {
+        this.apiUserService = apiUserService;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> createUser(@RequestBody ApiUserRegisterDTO dto) {
+        ApiUserRegisterDTO userSaved = apiUserService.registerUser(dto);
+        return new ResponseEntity<>(userSaved, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<?> updateUSer(@RequestBody ApiUserRegisterDTO dto) {
+        ApiUserRegisterDTO userUpdated = apiUserService.updateUser(dto);
+        return new ResponseEntity<>(userUpdated, HttpStatus.OK);
+    }
+
+    // Temporary Method.
+    @GetMapping()
+    public ResponseEntity<?> getUserByUsername(@RequestParam String username) {
+        ApiUser apiUser = apiUserService.getUserByUsername(username);
+
+        return new ResponseEntity<>(apiUser, HttpStatus.OK);
+    }
+
+    @DeleteMapping()
+    public ResponseEntity<?> deleteUserByUsername(@RequestParam String username) {
+        apiUserService.deleteUserByUsername(username);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
