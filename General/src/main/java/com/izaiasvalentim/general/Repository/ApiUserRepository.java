64cf9377fda05e7fe90c7bf900bd0d3f7f20a956
IzaiasValentim <izaias451@gmail.com>
2025-02-01 12:29:35 -0300

    feat/ Created some queries through the JPA repository.

--- General/src/main/java/com/izaiasvalentim/general/Repository/ApiUserRepository.java
+++ General/src/main/java/com/izaiasvalentim/general/Repository/ApiUserRepository.java
@@ -4,6 +4,11 @@ import com.izaiasvalentim.general.Domain.ApiUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ApiUserRepository extends JpaRepository<ApiUser, Long> {
+    Optional<ApiUser> findByUsername(String username);
+    Optional<ApiUser> findByEmail(String email);
+    Optional<ApiUser> findByCPF(String cpf);
 }
