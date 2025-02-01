a76b1cbd5576804a7c28a5fbbf467a71a65b584d
IzaiasValentim <izaias451@gmail.com>
2025-02-01 16:54:29 -0300

    fix/ Removed unused import.

--- General/src/main/java/com/izaiasvalentim/general/Repository/BaseUserRepository.java
+++ General/src/main/java/com/izaiasvalentim/general/Repository/BaseUserRepository.java
@@ -1,6 +1,5 @@
 package com.izaiasvalentim.general.Repository;
 
-import com.izaiasvalentim.general.Domain.ApiUser;
 import com.izaiasvalentim.general.Domain.BaseUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
