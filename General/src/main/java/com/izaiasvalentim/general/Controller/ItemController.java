436f4321708c48490d415760b972895ef98c6616
IzaiasValentim <izaias451@gmail.com>
2025-02-03 13:50:51 -0300

    feat/ Implemented role verification to access routes (Authorization).

--- General/src/main/java/com/izaiasvalentim/general/Controller/ItemController.java
+++ General/src/main/java/com/izaiasvalentim/general/Controller/ItemController.java
@@ -7,6 +7,7 @@ import com.izaiasvalentim.general.Service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,6 +22,10 @@ public class ItemController {
     }
 
     @GetMapping(value = "allByName")
+    @PreAuthorize(
+                    "hasAuthority('SCOPE_MANAGER') || " +
+                    "hasAuthority('SCOPE_SELLER') || " +
+                    "hasAuthority('SCOPE_INTERN') ")
     public ResponseEntity<?> getAllItems(@RequestParam String name) {
         var listItems = itemService.getAllItemsByName(name);
 
@@ -28,6 +33,7 @@ public class ItemController {
     }
 
     @PostMapping(value = "/")
+    @PreAuthorize("hasAuthority('SCOPE_MANAGER') || hasAuthority('SCOPE_INTERN')")
     public ResponseEntity<?> addItem(@RequestBody ItemDTO dto) {
         var item = Item.itemDTOToItem(dto);
         var savedItem = itemService.registerNewItem(item);
@@ -36,12 +42,14 @@ public class ItemController {
     }
 
     @PostMapping(value = "addStockByName/")
+    @PreAuthorize("hasAuthority('SCOPE_MANAGER') || hasAuthority('SCOPE_INTERN')")
     public ResponseEntity<?> addItemStockByName(@RequestBody ItemAddStockDTO dto) {
         var savedItem = itemService.addItemStock(dto);
         return new ResponseEntity<>(savedItem, HttpStatus.CREATED);
     }
 
     @DeleteMapping(value = "deleteByBatch/")
+    @PreAuthorize("hasAuthority('SCOPE_MANAGER') || hasAuthority('SCOPE_INTERN')")
     public ResponseEntity<?> deleteItemByBatch(@RequestParam String batch) {
         var isExcluded = itemService.deleteItemStockByBatch(batch);
 
