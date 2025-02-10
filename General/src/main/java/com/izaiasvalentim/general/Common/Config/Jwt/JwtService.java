a9864b0eb74ec1243b672c340aea90c4b384329d
IzaiasValentim <izaias451@gmail.com>
2025-02-10 10:48:47 -0300

    fix/ Update for centralized time data retrieval.

--- General/src/main/java/com/izaiasvalentim/general/Common/Config/Jwt/JwtService.java
+++ General/src/main/java/com/izaiasvalentim/general/Common/Config/Jwt/JwtService.java
@@ -1,6 +1,7 @@
 package com.izaiasvalentim.general.Common.Config.Jwt;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
@@ -11,9 +12,13 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.util.stream.Collectors;
 
+import static com.izaiasvalentim.general.Common.utils.TimeUtils.getCurrentTimeTruncatedInSeconds;
+
 @Service
 public class JwtService {
     private final JwtEncoder jwtEncoder;
+    @Value("${tokens.expiration.access.exp}")
+    private Integer accessTokenExpiresInSeconds;
 
     @Autowired
     public JwtService(JwtEncoder jwtEncoder) {
@@ -21,17 +26,18 @@ public class JwtService {
     }
 
     public String generateToken(Authentication authentication) {
-        Instant now = Instant.now();
-        long expiry = 180L;
+        Instant now = getCurrentTimeTruncatedInSeconds();
+        Instant expiration = now.plusSeconds(accessTokenExpiresInSeconds);
 
         String scopes = authentication.getAuthorities()
                 .stream()
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.joining(" "));
+
         var claims = JwtClaimsSet.builder()
                 .issuer("General")
                 .issuedAt(now)
-                .expiresAt(now.plusSeconds(expiry))
+                .expiresAt(expiration)
                 .subject(authentication.getName())
                 .claim("scope", scopes)
                 .build();
